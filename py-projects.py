18cb61bdafaf4d35c9d7cd9d362118ad66566755
prathikshroff1989 <40131048+prathikshroff1989@users.noreply.github.com>
2018-06-09 19:12:55 -0700

    Modify the code so that it stops printing when it reaches a number that is exactly divisible by 11.

--- py-projects.py
+++
@@ -1,4 +0,0 @@
-for i in range(0, 100, 7):
-    print(i)
-    if (i > 0) and (i % 11 == 0):
-       break
---
+++ py-projects1.py
@@ -0,0 +1,4 @@
+for i in range(0,21):
+    if (i % 3 == 0) or (i % 5 == 0):
+        continue
+    print(i)
